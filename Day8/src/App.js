f2b7de6c8141816f4a396b94f8fee4c48917e120
Yosua Ian <yosua.ian.sebastian@gmail.com>
2018-01-24 16:13:36 +0700

    Same task, new way. Using react to render Todos

--- Day8/src/App.js
+++ Day8/src/App.js
@@ -2,13 +2,26 @@
 
 import React, {createElement} from 'react';
 
-function Todo(props) {
+// Rewrite all createElement with JSX - Done
+
+type ItemProps = {};
+
+function TodoItem(props: ItemProps) {
   let {item} = props;
 
   // JSX
   // Use curly braces to wrap any js thing like variable and function
   // You dont need any for regular old string
-  return <li onClick={() => console.log('clicked')}>{item.content}</li>;
+  return (
+    <li
+      onClick={() => {
+        item.isDone = !item.isDone;
+        console.log(item);
+      }}
+    >
+      {item.isDone ? <s>{item.content}</s> : item.content}
+    </li>
+  );
   // Equivalent to this
   // return createElement(
   //   'li',
@@ -21,12 +34,14 @@ function Todo(props) {
 
 function App(props: Object) {
   let {todoItems} = props;
-  return createElement(
-    'ul',
-    {},
-    todoItems.map((item) => {
-      return createElement(Todo, {item});
-    }),
+
+  return (
+    <ul>
+      {todoItems.map((item) => {
+        //  return createElement(Todo, {item});
+        return <Todo key={item.id} item={item} />;
+      })}
+    </ul>
   );
 }
 
--- Day8/src/main.js
+++ Day8/src/main.js
@@ -1,22 +1,32 @@
 // @flow
+// react is universal
+import React from 'react';
+// dealing with browers with dom
+import ReactDOM from 'react-dom';
+// for mobile, use react-native
 
-import {createElement} from 'react';
-import {render} from 'react-dom';
 import App from './App';
-
-let todoItems = [
-  {id: '4', content: 'Buy oreos', isDone: true},
-  {id: '5', content: 'Eat oreos', isDone: false},
-  {id: '7', content: 'Buy more oreos', isDone: true},
-];
 
 // createElement takes 3 args or more
 // 1. The type, li, p, div, or even App and Todo
 // 2. The props, onClick, isWrong, etc...
 // 3. The children, could be another createElement or just a string, haha
-render(createElement(App, {todoItems}), document.body);
 
-let el = {
-  type: App,
-  props: 'todoitems',
+let body = document.body;
+
+if (body) {
+  function render() {
+    ReactDOM.render(<App state={state} />, body);
+  }
+  render();
+}
+
+let state = {
+  todoItems: [
+    {id: '4', content: 'Buy oreos', isDone: true},
+    {id: '5', content: 'Eat oreos', isDone: false},
+    {id: '7', content: 'Buy more oreos', isDone: true},
+  ],
 };
+
+export function set() {}
